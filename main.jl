a005574108330bca70addf68923bb15ff669f341
AlinaTsvetkov <tsvetkovalina1@gmail.com>
2026-09-20 01:11:00 -0500

    Project Completed!

--- main.jl
+++ main.jl
@@ -40,12 +40,13 @@ mutable struct Sensor{T<:Number}
     end
 end
 
-struct Vehicle{T<:Number, S<:Sensor}
-    name::String
-    battery::Battery
-    position::Vector{T}
-    orientation::Vector{T}
-    sensors::Dict{String, S}
+mutable struct Vehicle{T<:Number, S<:Sensor}
+    operations_completed::Int64
+    const name::String
+    const battery::Battery
+    const position::Vector{T}
+    const orientation::Vector{T}
+    const sensors::Dict{String, S}
 
     function Vehicle(name::String, battery::Battery, position::Vector{T}, orientation::Vector{T}, sensors::Dict{String, S}) where {T<:Number} where {S<:Sensor}
         normalized_name = titlecase(strip(name))
@@ -66,7 +67,7 @@ struct Vehicle{T<:Number, S<:Sensor}
             error("Vehicle must have at least 1 sensor")
         end
 
-        return new{T, S}(normalized_name, battery, position, orientation, sensors)
+        return new{T, S}(0, normalized_name, battery, position, orientation, sensors)
     end
 end
 
@@ -167,35 +168,128 @@ struct Crater
     landform::Landform
 end
 
-function operate(robot::Robot, plain::Plain)
+function operate!(robot::Robot, plain::Plain)
+    consume_energy!(robot.vehicle, 100)
     println("$(uppercase(robot.vehicle.name)): Starting operation with $(robot.tool) in the plain: $(plain.landform.name)")
+    show_operation_completed_message(robot.vehicle)
 end
 
-function operate(rover::Rover, plain::Plain)
+function operate!(rover::Rover, plain::Plain)
+    consume_energy!(rover.vehicle, 100)
     println("$(uppercase(rover.vehicle.name)): Starting operation with traction $(rover.traction) in the plain: $(plain.landform.name)")
+    show_operation_completed_message(rover.vehicle)
 end
 
-function operate(rover::Rover, rocky_terrain::RockyTerrain)
+function operate!(rover::Rover, rocky_terrain::RockyTerrain)
+    consume_energy!(rover.vehicle, 200)
     println("$(uppercase(rover.vehicle.name)): Starting operation with traction $(rover.traction) in the rocky terrain: $(rocky_terrain.landform.name)")
+    show_operation_completed_message(rover.vehicle)
 end
 
-function operate(rover::Rover, crater::Crater)
+function operate!(rover::Rover, crater::Crater)
+    consume_energy!(rover.vehicle, 300)
     println("$(uppercase(rover.vehicle.name)): Starting operation with traction $(rover.traction) in the crater: $(crater.landform.name)")
+    show_operation_completed_message(rover.vehicle)
 end
 
-function operate(drone::Drone, crater::Crater)
+function operate!(drone::Drone, crater::Crater)
+    consume_energy!(drone.vehicle, 300)
     println("$(uppercase(drone.vehicle.name)): Starting operation at maximum altitude $(drone.max_altitude) in the crater: $(crater.landform.name)")
+    show_operation_completed_message(drone.vehicle)
 end
 
-atlas_battery = Battery(2000, 5000)
+function consume_energy!(vehicle::Vehicle, energy_to_consume::Int64)
+    if vehicle.battery.charge < energy_to_consume
+        error("$(uppercase(vehicle.name)): I can't do this operation because I'm out of battery")
+    end
+
+    vehicle.battery.charge -= energy_to_consume
+    vehicle.operations_completed += 1
+
+    println("$(uppercase(vehicle.name)): I've consumed $(energy_to_consume) after doing the operation. Now my charge is: $(vehicle.battery.charge)/$(vehicle.battery.capacity)")
+end
+
+function charge_energy!(vehicle::Vehicle)
+    if vehicle.battery.charge == vehicle.battery.capacity
+        error("$(uppercase(vehicle.name)): I can't do this operation because my charge is full!")
+    end
+
+    sleep(1.5)
+    
+    vehicle.battery.charge = vehicle.battery.capacity
+    println("$(uppercase(vehicle.name)): I've charged my battery again! Now my charge is: $(vehicle.battery.charge)/$(vehicle.battery.capacity)")
+end
+
+function show_operation_completed_message(vehicle::Vehicle)
+    sleep(1.5)
+
+    println("$(uppercase(vehicle.name)): I've finished the operation successfully!")
+end
+
+function show_operations_report(vehicles::Dict{String, T}) where {T<:Vehicle}
+    print("\n========= MISSION REPORT =========\n\n")
+
+    for vehicle in values(vehicles)
+        print("""
+        VEHICLE: $(vehicle.name)
+        OPERATIONS COMPLETED: $(vehicle.operations_completed)
+        REMAINING BATTERY: $(vehicle.battery.charge)
+        BATTERY: CAPACITY: $(vehicle.battery.capacity)
+        \n""")
+    end
+end
+
+
+#ATLAS CONFIGURATION
+atlas_battery = Battery(4500, 5000)
 atlas_temperature = Sensor(24.5, "Temperature", "°C")
 atlas_sensors = Dict(
     "Temperature" => atlas_temperature
 )
 
-atlas_base = Vehicle(atlas_battery, atlas_sensors)
+atlas_base = Vehicle("Atlas", atlas_battery, atlas_sensors)
 atlas = Robot(atlas_base, "Drill")
 atlas_base_location = Landform("North Valley")
 atlas_location = Plain(atlas_base_location)
 
-operate(atlas, atlas_location)
+operate!(atlas, atlas_location)
+
+#FREYA CONFIGURATION
+
+freya_battery = Battery(7000, 15000)
+freya_temperature = Sensor(21.3, "Temperature", "°C")
+freya_sensors = Dict(
+    "Temperature" => freya_temperature
+)
+
+freya_base = Vehicle("Freya", freya_battery, freya_sensors)
+freya = Rover(freya_base, 0.84)
+freya_base_location = Landform("Diamond Crater")
+freya_location = Crater(freya_base_location)
+
+operate!(freya, freya_location)
+charge_energy!(freya.vehicle)
+
+#NYX CONFIGURATION
+
+nyx_battery = Battery(3000, 3500)
+nyx_temperature = Sensor(26.5, "Temperature", "°C")
+nyx_sensors = Dict(
+    "Temperature" => nyx_temperature
+)
+
+nyx_base = Vehicle("Nyx", nyx_battery, nyx_sensors)
+nyx = Drone(nyx_base, 43.23)
+nyx_base_location = Landform("Magma Crater")
+nyx_location = Crater(nyx_base_location)
+
+operate!(nyx, nyx_location)
+
+#CONFIGURING DICTIONARY
+vehicles = Dict(
+    "Atlas" => atlas.vehicle,
+    "Freya" => freya.vehicle,
+    "Nyx" => nyx.vehicle,
+)
+
+show_operations_report(vehicles)
